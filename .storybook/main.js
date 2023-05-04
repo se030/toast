f41f0d163550d93e6d124576a22c46b4e3d5d729
se <63814960+se030@users.noreply.github.com>
2023-05-04 19:57:18 +0900

    Test: Add UI Interaction Test (#1)
    * chore: add storybook configuration for interaction testing
    
    https://storybook.js.org/tutorials/ui-testing-handbook/react/ko/interaction-testing/
    
    * refactor: separate ToastForm component for story readability
    
    * chore: upgrade storybook to v7
    
    * test: add simple interaction test
    
    * fix: set Toast role to status
    
    - live region containing advisory information for the user that is not important enough to be an alert
    - https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/status_role
    
    * chore: set Storybook autodocs to false
    
    to ensure each story has just one ToastContainer
    
    * feat: modify Toast story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,31 @@
-const viteTsconfig = require('vite-tsconfig-paths');
-const tsconfigPaths = viteTsconfig.default;
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const { mergeConfig } = require('vite');
 
-module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+const config = {
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
   ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-vite',
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
+  core: {},
   features: {
     storyStoreV7: true,
+    interactionsDebugger: true,
   },
   async viteFinal(config) {
     return mergeConfig(config, {
       plugins: [tsconfigPaths()],
     });
   },
+  docs: {
+    autodocs: false,
+  },
 };
+
+export default config;
